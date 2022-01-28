18a5a64659cf46cebed72cef88ff3703dd4623b0
Artemis <artemis@townland.xyz>
2022-01-28 13:55:23 +0330

    input component redesigned

--- src/components/input.js
+++ src/components/input.js
@@ -1,8 +1,27 @@
 module.exports = {
-    '.tl-input': {
+    '.tl-input-container': {
+        'display': 'inline-flex',
+        'flex-wrap': 'nowrap',
+        'align-items': 'center',
         'border': '2px solid',
-        'border-radius': '8px',
+        'border-color': 'var(--tl-defaults-border-color, #303952)',
+        'border-radius': 'var(--tl-defaults-radius, 6px)',
         'padding': '8px 14px',
-        'position': 'relative',
+
+        'label': {
+            'display': 'flex',
+            'flex-direction': 'column',
+            'font-size': '12px',
+            'color': 'var(--tl-defaults-border-color, #303952)',
+        },
+
+        'input': {
+            'background-color': 'transparent',
+            'font-size': '14px',
+
+            '&::placeholder': {
+                'color': 'var(--tl-defaults-placeholder-color, #57606F)',
+            }
+        }
     }
 }
--- src/styles/root.js
+++ src/styles/root.js
@@ -1,4 +1,5 @@
 module.exports = {
     '--tl-defaults-radius': '6px',
     '--tl-defaults-border-color': '#303952',
+    '--tl-defaults-placeholder-color': '#57606F',
 }
